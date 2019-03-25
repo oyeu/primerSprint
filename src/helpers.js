ee7a21c469c67df66293d2b661e5480eeebfd830
oyeu <daarangovi@gmail.com>
2019-03-25 15:44:16 -0500

    cambio5

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,5 @@
 const hbs =  require('hbs');
-const usuario = require('./usuario')
+const usuario = require('./usuario');
+const curso = require('./cursos');
 
 hbs.registerHelper('crearUsuario',usuario.crear)
