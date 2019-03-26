3d6756cb78cc92d9533dcdefbc92038066e85e7b
oyeu <daarangovi@gmail.com>
2019-03-26 16:42:34 -0500

    correcion

--- src/helpers.js
+++ src/helpers.js
@@ -7,40 +7,6 @@ hbs.registerHelper('crearCurso', curso.crearcurso);
 
 hbs.registerHelper('listarCursos', () => {
     listadoCursos = require('./listadoCursos.json')
-    if (!listadoCursos.length) {
-      return "no hay cursos creados"
-    }else {
-      let texto = "<table> \
-      <thead> \
-      <th> Nombre </th> \
-      <th> Id </th> \
-      <th> Descripción </th> \
-      <th> Valor </th> \
-      <th> Modalidad </th> \
-      <th> Intensidad horaria </th> \
-      <th> estado </th> \
-      </thead> \
-      <tbody>";
-
-      listadoCursos.forEach(curso => {
-        texto = texto +
-<<<<<<< HEAD
-            '<tr>' +
-            '<td>' + curso.nombre + '</td>' +
-            '<td>' + curso.id + '<tyd>' +
-            '<td>' + curso.descripcion + '</td>' +
-            '<td>' + curso.valor + '</td>' +
-            '<td>' + curso.modalidad + '</td>' +
-            '<td>' + curso.ih + '</td>' +
-            '<td>' + curso.estado + '</td></tr>';
-
-    })
-    texto = texto + '</tbody></table>';
-    return texto;
-})
-
-hbs.registerHelper('listarCursos', () => {
-    listadoCursos = require('./listadoCursos.json')
     let texto = "<table class='table table-striped'> \
              <thead class='thead-dark'> \
              <th> Nombre </th> \
@@ -63,22 +29,10 @@ hbs.registerHelper('listarCursos', () => {
             '<td>' + curso.modalidad + '</td>' +
             '<td>' + curso.ih + '</td>' +
             '<td>' + curso.estado + '</td></tr>';
-=======
-        '<tr>' +
-        '<td>' + curso.nombre + '</td>' +
-        '<td>' + curso.id + '<tyd>' +
-        '<td>' + curso.descripcion + '</td>' +
-        '<td>' + curso.valor + '</td>' +
-        '<td>' + curso.modalidad + '</td>' +
-        '<td>' + curso.ih + '</td>' +
-        '<td>' + curso.estado + '</td></tr>';
->>>>>>> 5d6c2604010b3f8ee70df7dd3264bb4f33e84c98
-
       })
       texto = texto + '</tbody></table>';
       return texto;
-    }
-})
+    })
 
 hbs.registerHelper('listarDisponibles', () => {
     listadoCursos = require('./listadoCursos.json')
